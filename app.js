dd6e2f71ee477ae735476c32bd31a00d355467b9
potatochunks <a2545585912@163.com>
2022-01-24 11:06:17 +0800

    api接口测试

--- app.js
+++
@@ -1,7 +0,0 @@
-import { NowRequest, NowResponse } from '@vercel/node';
-module.exports = async (NowRequest, NowResponse) => {
-    var data = {
-        msg: "hello world!"
-    }
-    NowResponse.status(200).json(data);
-}
