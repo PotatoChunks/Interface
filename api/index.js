b68688ada7575b5f159ba839eef21351bb1db62e
potatochunks <a2545585912@163.com>
2022-01-24 14:17:46 +0800

    3000

--- api/index.js
+++ api/index.js
@@ -22,9 +22,16 @@ app.use((req, res, next) => {
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(express.static(path.join(__dirname, '../public')))
+//app.use(express.static(path.join(__dirname, '../public')))
 
 //设置路由
 app.use('/', require('./routes/index'))
+
+const port = process.env.PORT || 3000
+const host = process.env.HOST || ''
+
+app.server = app.listen(port, host, () => {
+    console.log(`server running @ http://${host ? host : 'localhost'}:${port}`)
+})
 
 module.exports = app
--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../../public/index.html"))
+    res.send({code:0,msg:"成功"})
 })
 
 
