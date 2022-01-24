9bbead2cd30efb220ddb4040a8396608e968eea8
potatochunks <a2545585912@163.com>
2022-01-24 14:38:52 +0800

    a.js

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get("/",(req,res)=>{
-    res.send({code:0,msg:"成功"})
+router.post("/test",(req,res)=>{
+    res.sendFile("aaa.js")
 })
 
 
