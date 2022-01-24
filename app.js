5324a09ec822932efa19d70298e45bbcec6f43a9
potatochunks <a2545585912@163.com>
2022-01-24 10:34:22 +0800

    监听测试接口

---
+++ app.js
@@ -0,0 +1,7 @@
+let http = require('http')
+let server = http.createServer((req,res)=>{
+    res.writeHead(200,{'Contetn-type':'text/html;charset=UTF-8'})
+    res.write('<h1>Hellow</h1>')//写入
+    res.end('hellow')//结束
+})
+server.listen(200,'localhost')//监听端口
