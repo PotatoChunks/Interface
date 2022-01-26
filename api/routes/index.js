a1b5e8af85262b47fb30a14016f6f98a0bfe17ef
potatochunks <a2545585912@163.com>
2022-01-26 14:07:19 +0800

    添加session

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,8 +3,6 @@ const router = express.Router()
 const fs = require("fs")
 
 router.get("/test",(req,res)=>{
-    let user = {name:"狗蛋",age:18,status:true}
-    fs.writeFileSync("./aa.txt",JSON.stringify(user),{encoding:"utf8"})
     res.send({
         code:0,
         msg:"成功"
@@ -12,8 +10,6 @@ router.get("/test",(req,res)=>{
 })
 
 router.get("/read",(req,res)=>{
-    let data = ""
-    data = fs.readFileSync("./aa.txt","utf8")
     res.send({
         code:0,
         msg:"成功",
