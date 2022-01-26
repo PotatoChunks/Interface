778c632b84f87f08ae2703bec5c2baffc8649495
potatochunks <a2545585912@163.com>
2022-01-26 11:17:31 +0800

    读写文件

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const fs = require("fs")
 
 router.get("/test",(req,res)=>{
-    res.sendFile("/aaa.js")
+    fs.writeFill("./test.txt","Hello",{encoding:"utf8"},(err)=>{})
+    res.send({
+        code:0,
+        msg:"成功"
+    })
+})
+
+router.get("/read",(req,res)=>{
+    let data = ""
+    data = fs.readFileSync("./test.txt","utf8")
+    res.send({
+        code:0,
+        msg:"成功",
+        data
+    })
 })
 
 
