8cd459f9087f4dddde3320103598503798ec6f45
Potato chunks <55194014+PotatoChunks@users.noreply.github.com>
2022-02-03 16:21:19 +0800

    Delete db.js

--- api/db/db.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose')
-
-const uri = "mongodb+srv://root:root@mongodb1.bwdc8.mongodb.net/admin?retryWrites=true&w=majority";
-mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true})
