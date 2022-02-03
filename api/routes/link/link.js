845e6523a136ce40d1a8ff7a526265cd63146603
potatochuks <A2545585912@163.com>
2022-02-03 11:23:42 +0800

    返回数据库数据user

--- api/routes/link/link.js
+++ api/routes/link/link.js
@@ -13,8 +13,8 @@ const uri = "mongodb+srv://<admin>:<admin>@mongodb1.bwdc8.mongodb.net/myMongodb?
 //
 router.get("/",async (req,res)=>{
     const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    const db = await client.db('[sample_analytics]');
-    var result = await db.collection("[accounts]").find().toArray();
+    const db = await client.db('[test]');
+    var result = await db.collection("[user]").find().toArray();
     res.status(200).json(result);
 })
 
