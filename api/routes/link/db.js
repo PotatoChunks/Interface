d5a175e51f9b8b1511de3766d7080884f18a9bf6
potatochuks <A2545585912@163.com>
2022-02-03 10:48:54 +0800

    连接数据库并返回信息

--- api/routes/link/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://<admin>:<admin>@mongodb1.bwdc8.mongodb.net/sample_airbnb?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=>{console.log('连接数据库成功')})
-.catch(()=>{console.log('连接数据库失败')})
--- api/routes/link/link.js
+++ api/routes/link/link.js
@@ -5,11 +5,18 @@
 const express = require('express')
 const router = express.Router()
 
-const mongodb = require("./db");
+//const mongodb = require("./db");
+
+const mongoose = require("mongoose");
+
+
 
 // 
-router.get("/",(req,res)=>{
-    res.send({code:0})
+router.get("/",async (req,res)=>{
+    let client = await mongoose.connect("mongodb+srv://<admin>:<admin>@mongodb1.bwdc8.mongodb.net/sample_airbnb?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true})
+    let db = await client.db('[sample_airbnb]')
+    let value = await db.addCollection('[listingsAndReviews]').find().toArray()
+    res.send({code:0,data:value})
 })
 
 
