56c3571cc981e06b1331e24f7b03bb5e3bdbbf93
potatochuks <A2545585912@163.com>
2022-02-03 10:57:08 +0800

    连接数据库并返回信息

--- api/routes/link/link.js
+++ api/routes/link/link.js
@@ -9,14 +9,42 @@ const router = express.Router()
 
 const mongoose = require("mongoose");
 
+let client = mongoose.connect("mongodb+srv://<admin>:<admin>@mongodb1.bwdc8.mongodb.net/sample_airbnb?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true})
+
+let Schema = mongoose.Schema;//准备建表
+let userSchema = new Schema({
+    //key 以及规则
+    username : {type:String,required:true},//必须有username
+    password : {type:String,required:true}
+})
+let user = mongoose.model("user",userSchema)
 
 
-// 
+
+//
 router.get("/",async (req,res)=>{
-    let client = await mongoose.connect("mongodb+srv://<admin>:<admin>@mongodb1.bwdc8.mongodb.net/sample_airbnb?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true})
-    let db = await client.db('[sample_airbnb]')
-    let value = await db.addCollection('[listingsAndReviews]').find().toArray()
-    res.send({code:0,data:value})
+    user.create({
+        username:"aaa",
+        password:"bbb"
+    }).then(d=>{
+        console.log("成功")
+        res.send({
+            code:0,
+            msg:"成功"
+        })
+        return
+    }).catch(e=>{
+        console.log("失败")
+        res.send({
+            code:1,
+            msg:"失败"
+        })
+        return
+    })
+    res.send({
+        code:500,
+        msg:"无法连接数据库"
+    })
 })
 
 
