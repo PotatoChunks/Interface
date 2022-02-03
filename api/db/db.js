d1f52acfc468d2c41e4da12a028162cbcb4192eb
Potato chunks <55194014+PotatoChunks@users.noreply.github.com>
2022-02-03 15:51:29 +0800

    Update db.js

--- api/db/db.js
+++ api/db/db.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 
-const uri = "mongodb+srv://admin:admin@mongodb1.bwdc8.mongodb.net/admin?retryWrites=true&w=majority";
+const uri = "mongodb+srv://root:root@mongodb1.bwdc8.mongodb.net/admin?retryWrites=true&w=majority";
 mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true})
