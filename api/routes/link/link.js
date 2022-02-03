6b5fa2b6256f3aa49f31544533c4e19ca0210d95
potatochuks <A2545585912@163.com>
2022-02-03 12:06:45 +0800

    user

--- api/routes/link/link.js
+++ api/routes/link/link.js
@@ -13,8 +13,8 @@ const uri = "mongodb+srv://<admin>:<admin>@mongodb1.bwdc8.mongodb.net/admin?retr
 //
 router.get("/",async (req,res)=>{
     const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    const db = await client.db('[test]');
-    var result = await db.collection("[user]").find().toArray();
+    const db = await client.db('test');
+    var result = await db.collection("user").find().toArray();
     res.status(200).json(result);
 })
 
