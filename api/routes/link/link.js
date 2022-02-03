4cf0905a2a1c6d4c2de03f24bac6db29ff611871
potatochuks <A2545585912@163.com>
2022-02-03 11:01:21 +0800

    demo

--- api/routes/link/link.js
+++ api/routes/link/link.js
@@ -7,40 +7,12 @@ const router = express.Router()
 
 //const mongodb = require("./db");
 
-const mongoose = require("mongoose");
-
-let client = mongoose.connect("mongodb+srv://<admin>:<admin>@mongodb1.bwdc8.mongodb.net/sample_airbnb?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true})
-
-let Schema = mongoose.Schema;//准备建表
-let userSchema = new Schema({
-    //key 以及规则
-    username : {type:String,required:true},//必须有username
-    password : {type:String,required:true}
-})
-let user = mongoose.model("user",userSchema)
 
 
 
 //
 router.get("/",async (req,res)=>{
-    user.create({
-        username:"aaa",
-        password:"bbb"
-    }).then(d=>{
-        console.log("成功")
-        res.send({
-            code:0,
-            msg:"成功"
-        })
-        return
-    }).catch(e=>{
-        console.log("失败")
-        res.send({
-            code:1,
-            msg:"失败"
-        })
-        return
-    })
+
     res.send({
         code:500,
         msg:"无法连接数据库"
