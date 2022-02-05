577a6b8b1312d25e5564b0329d45579008390132
potatochuks <A2545585912@163.com>
2022-02-05 13:02:56 +0800

    删除session存储数据库

--- api/routes/blog/blog.js
+++ api/routes/blog/blog.js
@@ -18,14 +18,7 @@ router.use(logger('dev'));
 router.use(cookieParser());
 
 //设置session后端cookie ,token
-router.use(session({
-    secret:"potatoChunks"//秘钥, 一个字符, 用于加密, 不用自己解密可以自己随便写
-    ,cookie:{maxAge:2*60*60*1000}//给前端设置的cookie有效时间:2小时
-    ,rolling:true //用户和页面交互的时候刷新cookie时间
-    ,resave:false //是否每次重新存储session
-    ,saveUninitialized:false //初始化
-    ,store : new connectMongo({url : "mongodb+srv://root:root@mongodb1.bwdc8.mongodb.net/blog_db?retryWrites=true&w=majority"})//将session存储到数据库blog里
-}));
+
 
 //路由设置
 
