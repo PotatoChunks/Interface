791e7c6db329ac924959fe8c939a6a606a63a1ab
potatochuks <A2545585912@163.com>
2022-02-05 12:14:12 +0800

    添加链接

--- api/db/blogDb/27017.js
+++ api/db/blogDb/27017.js
@@ -7,5 +7,5 @@ mongoose.disconnect();
 //     .catch(e=>{
 //         console.log("连接失败");
 //     });
-eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('5.6("1+7://2:2@8.9.1.a/c?d=0&f=g",{h:0,i:0}).j(b=>{3.4(\'数据库连接成功\')}).k(e=>{3.4("连接失败")});',21,21,'true|mongodb|root|console|log|mongoose|connect|srv|mongodb1|bwdc8|net||blog_db|retryWrites||w|majority|useNewUrlParser|useUnifiedTopology|then|catch'.split('|'),0,{}))
-//mongodb+srv://!:!!!@mongodb1.bwdc8.mongodb.net/blog?retryWrites=true&w=majority
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('5.6("1+7://2:2@8.9.1.a/c?d=0&f=g",{h:0,i:0}).j(b=>{3.4(\'数据库连blog_db接成功\')}).k(e=>{3.4("连接blog_db失败")});',21,21,'true|mongodb|root|console|log|mongoose|connect|srv|mongodb1|bwdc8|net||blog_db|retryWrites||w|majority|useNewUrlParser|useUnifiedTopology|then|catch'.split('|'),0,{}))
+//mongodb+srv://!:!!!@mongodb1.bwdc8.mongodb.net/blog_db?retryWrites=true&w=majority
--- api/routes/link/link.js
+++ api/routes/link/link.js
@@ -15,6 +15,10 @@ router.get("/",async (req,res)=>{
         title:"百度",
         href:"https://www.baidu.com",
         describe:"百度一下你就知道"
+    }).then(_=>{
+        console.log('链接添加成功')
+    }).catch(_=>{
+        console.log('链接添加失败')
     })
     let {user,pwd} = req.query;
     res.send({
