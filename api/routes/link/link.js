c4e5ea2fd4893dc3192ab10348e6ed273c9ed825
potatochuks <A2545585912@163.com>
2022-02-05 13:17:49 +0800

    get

--- api/routes/link/link.js
+++ api/routes/link/link.js
@@ -17,9 +17,15 @@ router.get("/",async (req,res)=>{
         href:href,
         describe:"百度一下你就知道"
     }).then(_=>{
-        console.log('链接添加成功')
+        res.send({
+            code:0,
+            msg:"成功"
+        })
     }).catch(_=>{
-        console.log('链接添加失败')
+        res.send({
+            code:500,
+            msg:"失败"
+        })
     })
 })
 
@@ -28,6 +34,11 @@ router.get('/get',async (req,res)=>{
     await listList.find({}).then(data=>{
         res.send({
             data
+        })
+    }).catch(_=>{
+        res.send({
+            code:500,
+            msg:"失败"
         })
     })
 })
