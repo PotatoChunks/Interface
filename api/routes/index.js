9280524e1888f412f293ea8f1ddc97840c6229e2
potatochunks <a2545585912@163.com>
2022-03-01 09:27:09 +0800

    1

--- api/routes/index.js
+++ api/routes/index.js
@@ -9,10 +9,10 @@ router.use("/insect",require('./insect/insect'));
 router.use('/game',require('./game/index'));
 
 // 链接展示
-router.use('/link',require('./link/link'));
+//router.use('/link',require('./link/link'));
 
 // 个人博客
-router.use('/blog',require('./blog/blog'));
+//router.use('/blog',require('./blog/blog'));
 
 // ico图标
 router.get("/favicon.ico",(req,res)=>{
