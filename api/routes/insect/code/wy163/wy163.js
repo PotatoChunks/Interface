f01fa29e66bba4ca465eab5e2e7ce11cae3eaa2c
ZhouJIaWei <ZhouJIaWei@youmi.com>
2022-05-17 18:32:06 +0800

    验证码请求

--- api/routes/insect/code/wy163/wy163.js
+++ api/routes/insect/code/wy163/wy163.js
@@ -5,6 +5,27 @@ const express = require('express')
 const router = express.Router()
 const got = require('got')
 
-router.get('/',(req,res)=>{})
+const codeUrl = [
+    {
+        url:'https://n.cg.163.com/api/v1/phone-captchas/86-',
+        time:20
+    }
+]
+
+function foo(phone){
+    codeUrl.forEach(async item=>{
+        let data = await request.post(item.url+phone);
+        //console.log(item);
+    })
+}
+
+router.get('/',(req,res)=>{
+    let {phone} = req.query;
+    foo(phone);
+    res.send({
+        code:0,
+        msg:'成功'
+    })
+})
 
 module.exports = router
